5b2eb289107cbcfcfa31b8e1e013d9e14476e7a1
ganavi133 <110333410+ganavi133@users.noreply.github.com>
2024-11-24 19:36:13 +0530

    Revert "test commit"

--- src/util/Header.js
+++ src/util/Header.js
@@ -22,9 +22,7 @@ function Header() {
           style={{ display: "flex", justifyContent: "center" }}
           className="name"
         >
-          <h1 style={{ marginTop: "-30px", marginLeft: "-40px" }}>
-            Career Crafter
-          </h1>
+          <h1 style={{ marginTop: "-30px", marginLeft: "-40px" }}>Career Crafter</h1>
         </div>
         <div
           style={{
@@ -53,9 +51,7 @@ function Header() {
             <button className="btn" onClick={handleLoginClick}>
               Login
             </button>
-            <button className="btn" onClick={handleSignupClick}>
-              Sign Up
-            </button>
+            <button className="btn" onClick={handleSignupClick}>Sign Up</button>
           </div>
           <div />
         </div>
