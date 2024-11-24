f6db826223649b364cff63cc41d4e92e8bb2a38f
Ganavi <ganavir199@gmail.com>
2024-11-24 19:47:48 +0530

    new commite

--- src/util/Header.js
+++ src/util/Header.js
@@ -48,9 +48,7 @@ function Header() {
             Contact Us
           </Link>
           <div style={{ display: "flex", gap: "12px", marginLeft: "40px" }}>
-            <button className="btn" onClick={handleLoginClick}>
-              Login
-            </button>
+            
             <button className="btn" onClick={handleSignupClick}>Sign Up</button>
           </div>
           <div />
